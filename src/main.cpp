342f86b702f89d8ded6199d1ab91d5fdac25c2ef
Matthias Behr <mbehr@mcbehr.de>
2015-02-27 20:40:11 +0100

    added git version

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,10 @@
+#include <stdio.h>
+
+#include "gitSha1.h"
+
 int main(int argc, char *argv[])
 {
+	printf("%s version %s, last git commit %s\n", argv[0], g_GIT_SHALONG, g_GIT_LAST_COMMIT_DATE);
+
 	return 0;
 }
