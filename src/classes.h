e1e83394dd386b1bf433334f6415c7925dff1cd1
Matthias Behr <mbehr@mcbehr.de>
2015-03-01 23:15:06 +0100

    more examples

--- src/classes.h
+++ src/classes.h
@@ -1,6 +1,10 @@
 #ifndef __classes_h__
 #define __classes_h__
 
+#include <pthread.h>
+#include <list>
+#include <string>
+
 class ClassA
 {
 public:
@@ -24,6 +28,83 @@ public:
 
 protected:
 	char *_str;
+};
+
+class ClassD
+{
+public:
+	ClassD() : _stop(false) { pthread_create(&_tid, 0, fn, this); };
+	~ClassD() { _stop = true; assert(0==pthread_join(_tid, 0)); };
+	static void* fn(void *arg) { ClassD *d = (ClassD*)arg; while (!d->_stop) { ++_nr_threads; --_nr_threads; }; return 0;};
+
+	pthread_t _tid;
+	bool _stop;
+	static int _nr_threads;
+};
+
+class ClassE
+{
+public:
+	ClassE(){};
+	void add(const std::string &r) { add(r.c_str()); };
+	void add(const char *item) { if (!in_cache(item)) _cache.push_front(item);};
+	bool in_cache(const char *item) {
+		for (auto it=_cache.cbegin(); it!= _cache.cend(); ++it) {
+			if (item == *it) return true;
+		}
+		return false;
+	};
+	size_t size() const { return _cache.size(); };
+
+protected:
+	std::list<const char *> _cache;
+};
+
+class ClassF
+{
+public:
+	ClassF() : _init(false) {};
+	void init() { _init = true; }
+	bool play() {if (!_init) return false; return true; }
+
+protected:
+	bool _init;
+};
+
+class ClassG
+{
+public:
+	ClassG(ClassF &interface) : _interface(interface) {};
+	bool run() { bool b = _interface.play(); if (!b) _interface.init();  return b;}
+protected:
+	ClassF &_interface;
+};
+
+class ClassH
+{
+public:
+	ClassH(const std::string &start, const std::string &cyclic) : _pos(0), _start(start), _cyclic(cyclic) {};
+	std::string read(unsigned int size) {
+		unsigned int opos = _pos;
+		std::string ret;
+		if (!size) return ret;
+		if (_pos<_start.length())
+			ret = _start.substr(_pos, size);
+		_pos += ret.length();
+		unsigned int remain = size - ret.length();
+		if (remain && _cyclic.length()>0) {
+			do {
+				ret.append(_cyclic, (_pos - _start.length()) % _cyclic.length(), remain);
+				remain = size - ret.length();
+				_pos = opos + size - remain;
+			} while(remain > 0);
+		}
+		return ret;}
+
+protected:
+	unsigned int _pos;
+	std::string _start;
+	std::string _cyclic;
 };
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
+#include <unistd.h>
 #include <list>
+#include <string>
 
 #include "gitSha1.h"
 
@@ -18,36 +21,55 @@
  * + ex4: strdup/free
  * + ex3: constructor / virtual functions
  * + ex5: missing copy constructor/assignment op. and ref. counts
- * - recursion
  * + ex6/7: std::list erase / iterator invalid
- * - std::list endless loop
- * - race conds
+ * + ex6/7: std::list endless loop
+ * + ex8: race conds
  * - pthread cancel async
  * - pthread cancel and semaphores
  * - shutdown / threads
  * - time complexity (O(n^2), qsort,...)
  * - watchdog / max. response time
- * - wrong sequences
  * - locks and thread prios / priority inversion
- * - wrong cache
- * - "on change only" (if (a!=b))
- * - partial reads
- * - missed start of com (serial,...)
+ * + ex12: wrong sequences (e.g. play before init/alloc,...)
+ * + ex10: wrong cache
+ * - "on change only" (if (a!=b) send_change )
+ * - partial reads e.g. from backend,...
+ * + ex13: missed start of com (serial,...)
  * - missing filesystems/interfaces at startup
- * - no more filehandles
+ * - no more filehandles for that process (RLIMIT_NOFILE)
  * - too much memory / too big alloc.
- * - alsa return values (or generic handling return codes)
+ * - alsa return values (or generic handling return codes) (how to handle/simulate
+ *	-EBADFD	PCM is not in the right state (SND_PCM_STATE_PREPARED or SND_PCM_STATE_RUNNING)
+ *	-EPIPE	an underrun occurred
+ *	-ESTRPIPE	a suspend event occurred (stream is suspended and waiting for an application recovery)
  * - blocking/non-blocking io
  * - wrong access rights (set)
  * - wrong tmp file location
- * - assertions/warnings (default behaviour)
- * - time changes (& timeouts)
- * - too many files included
+ * - assertions/warnings (with fallback to default behaviour)
+ * - time changes (& timeouts) (pthread_mutex_timedlock and stime)
+ * - too many files included in binary delivery
  * - debug symbols included
  * - persistent data / versioning
- * - rt behaviour and memory lock (avoid paging)
- *
- *
+ * - rt behaviour and memory lock (mlock/mlockall to avoid paging)
+ * - floating point rounding/comparison
+ * + ex11: signed/unsigned (if (iX - uY < 0 )...)
+ * + ex9: delete[] (=new Foo[10]) -> not all destructors being called
+ * - parameter parsing (scanf("%d", code);)
+ * - porting code to 64bit (time_t) (see e.g. http://www.viva64.com/en/a/0004/)
+ #define INVALID_RESULT (0xFFFFFFFFu)
+ size_t MyStrLen(const char *str) {
+	if (str == NULL)
+	return INVALID_RESULT;
+  ...
+  return n;
+  }
+  size_t len = MyStrLen(str);
+  if (len == (size_t)(-1))
+ * ptrdiff_t SetBitN(ptrdiff_t value, unsigned bitNum) {
+  ptrdiff_t mask = 1 << bitNum;
+  return value | mask;
+	}
+	SetBitN(0, 31) SetBitN(0,32)
 */
 
 // macros:
@@ -131,6 +153,82 @@ void ex7(int limit){
 		printf("*it = %d\n", *it);
 }
 
+int ClassD::_nr_threads;
+
+void ex8()
+{
+	assert(ClassD::_nr_threads==0);
+	{
+		ClassD d1;
+		ClassD d2;
+	}
+	assert(ClassD::_nr_threads==0);
+
+	ClassD *dx = new ClassD[2];
+	usleep(10ul*1000);
+	delete[] dx;
+	if (ClassD::_nr_threads!=0) printf("ex8:ClassD::_nr_threads=%d\n", ClassD::_nr_threads);
+	//while(ClassD::_nr_threads!=0) usleep(1000);
+	// wrong signal/wakeup handling
+}
+
+void ex9()
+{
+	ClassD *dx = new ClassD[1];
+	delete dx;
+}
+
+void ex10()
+{
+	ClassE cache;
+	cache.add("test1");
+	cache.add("test1");
+	cache.add(std::string("test1"));
+	printf("cache.size()=%d\n", cache.size());
+	assert(cache.in_cache("test1"));
+	std::string s1("test");
+	s1.append("1");
+	if (!cache.in_cache(s1.c_str()))
+		printf("ex10: couldn't find test1\n" );
+}
+
+void ex11()
+{
+	int ix=10;
+	unsigned int ux = 20;
+	if (ix-ux < 0) {
+		printf("ex11: <0\n");
+	} else
+		printf("ex11: >=0\n");
+}
+
+void ex12()
+{
+	ClassF interface;
+	ClassG device(interface);
+	bool ok;
+	do {
+		ok = device.run();
+		if (!ok) printf("ex12: not ok!\n");
+	} while (!ok);
+}
+
+void ex13(int scrap_pos)
+{
+	std::string header("deadbeef");
+	std::string payload("1234567890123456789012");
+	std::string cyclic = header;
+	cyclic.append(payload);
+	std::string scrap = payload.substr(scrap_pos);
+	ClassH device(scrap, cyclic);
+	bool header_found = false;
+	do {
+		std::string buf = device.read(10);
+		printf("ex13: %s\n", buf.c_str());
+		header_found = (buf.find(header) != std::string::npos);
+	} while (!header_found);
+}
+
 int main(int argc, char *argv[])
 {
 	printf("%s version %s, last git commit %s\n", argv[0], g_GIT_SHALONG, g_GIT_LAST_COMMIT_DATE);
@@ -143,6 +241,12 @@ int main(int argc, char *argv[])
 	ex6(2);
 //	ex6(3);
 	ex7(3);
-
+	ex8();
+//	ex9();
+	ex10();
+	ex11();
+	ex12();
+	ex13(0);
+//  ex13(3);
 	return 0;
 }
